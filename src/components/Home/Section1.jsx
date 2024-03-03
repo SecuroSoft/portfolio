1e8dd5b804b0b9305dc1dd47e58134bb07b4b66d
Yashit Maheshwary <yashitmaheshwary@gmail.com>
2024-03-03 16:36:25 +0530

    feat: added text animation

--- src/components/Home/Section1.jsx
+++ src/components/Home/Section1.jsx
@@ -1,7 +1,13 @@
+"use client";
+
 import React from "react";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Header from "../Header";
 import Navbar from "../Navbar";
+import { TextGenerateEffect } from "../ui/text-generate-effect";
+
+const words = `Crafting secure, intelligent digital experiences with cutting-edge AI innovation. From sleek designs to powerful development, we specialize in bringing your ideas to life in the ever-evolving tech realm. Committed to excellence, trust us to seamlessly amplify your vision.`;
+
 const Section1 = () => {
   return (
     <>
@@ -11,12 +17,11 @@ const Section1 = () => {
       <div className="pt-[71px]">
         <div className="space-y-4 mx-[20px]">
           <h1 className="text-[42px] lg:text-[80px] text-center font-semibold leading-[50px] lg:leading-[92px] text-clash-display">
-            Turning Ideas into 
-            <br />
+            <TextGenerateEffect words="Turning Ideas into" />
             <span className="linear-text-dark">Digital Reality, Seamlessly</span>
           </h1>
           <div className="max-w-[853px] w-full mx-auto text-center text-sm lg:text-lg leading-[28px]">
-          Crafting secure, intelligent digital experiences with cutting-edge AI innovation. From sleek designs to powerful development, we specialize in bringing your ideas to life in the ever-evolving tech realm. Committed to excellence, trust us to seamlessly amplify your vision.
+            <TextGenerateEffect words={words} />
           </div>
         </div>
         <button className="mt-[48px] border border-[#CAB7F7] mx-auto py-4 px-9 rounded-[36px] flex items-center">
